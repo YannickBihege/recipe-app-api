837c3bae3302e5a6857b9b4e8e30d59b237b8d76
YannickBihege <yannickbtheone@hotmail.com>
2025-02-22 16:04:12 +0100

    Remove, delete tests.py , a folder tests is created within the folder named core

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,11 @@
+"""
+Dajongo command to wait for the database to be available
+"""
+
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    """Django commad to wait for database"""
+
+    def handle(self, *args, **options):
+        pass
